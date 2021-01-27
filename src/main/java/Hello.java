d6a4eba9cf6735b3b1792d97b74269cb66903c5f
Sladjan1983 <74144698+Sladjan1983@users.noreply.github.com>
2021-01-26 20:13:58 -0500

    Added build plug in

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("B21 future SDET learning Git");
+        System.out.println("Added build plug in to make sure to always use Java 8 anywhere");
     }
 }
