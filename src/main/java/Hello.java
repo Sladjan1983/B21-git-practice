3fe6cc9d8de34460de43e25c20d081db2c3372c0
Sladjan1983 <74144698+Sladjan1983@users.noreply.github.com>
2021-01-26 22:00:47 -0500

    movie2 branch watching on the phone for 10 minutes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
         System.out.println("Hello World, B22");
         System.out.println("B21 future SDET learning Git");
         System.out.println("Added build plug in to make sure to always use Java 8 anywhere");
+        //movie on the phone 10 minute
     }
 }
