3892640c4352b468c271fca64a2b23a584163e2d
Sladjan1983 <74144698+Sladjan1983@users.noreply.github.com>
2021-01-26 22:03:34 -0500

    master branch watching on the living for 10 minutes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
         System.out.println("Hello World, B22");
         System.out.println("B21 future SDET learning Git");
         System.out.println("Added build plug in to make sure to always use Java 8 anywhere");
+
+        //oh like this movie who played it? let me continue
     }
 }
