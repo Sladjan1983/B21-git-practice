7c23f1c022dcad29a69dcf487ea5e4e9f2b06af3
Sladjan1983 <74144698+Sladjan1983@users.noreply.github.com>
2021-01-26 21:32:04 -0500

    movie 1 brunch updated Hello.Java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World, B21");
         System.out.println("B21 future SDET learning Git");
         System.out.println("Added build plug in to make sure to always use Java 8 anywhere");
     }
