79a36d5e99e365c6eb36dea6e886ca91f16b7210
Sladjan1983 <74144698+Sladjan1983@users.noreply.github.com>
2021-01-26 21:43:38 -0500

    movie1 branch final work is done, ready to merge not knowing spouse moved location of movie

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("B21 future SDET learning Git");
         System.out.println("Added build plug in to make sure to always use Java 8 anywhere");
         //watching more movie in bedroom
+        //ready to go back to living room
     }
 }
